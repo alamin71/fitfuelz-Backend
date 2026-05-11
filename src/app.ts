a1f2335ffb2d52f47efd85f21dcdad6e234575ff
Al-Amin <alamin50cse.miu@gmail.com>
2026-05-11 18:41:07 +0600

    following list updatd

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-import router from "./routes";
-import { Morgan } from "./shared/morgan";
-import globalErrorHandler from "./globalErrorHandler/globalErrorHandler";
-import { notFound } from "./app/middleware/notFound";
-import { welcome } from "../src/utils/welcome";
-import config from "./config";
-import path from "path";
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import router from './routes';
+import { Morgan } from './shared/morgan';
+import globalErrorHandler from './globalErrorHandler/globalErrorHandler';
+import { notFound } from './app/middleware/notFound';
+import { welcome } from './utils/welcome';
+import config from './config';
+import path from 'path';
 const app: Application = express();
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 //morgan
 app.use(Morgan.successHandler);
 app.use(Morgan.errorHandler);
@@ -18,7 +18,7 @@ app.use(Morgan.errorHandler);
 //body parser
 app.use(
   cors({
-    origin: config.allowed_origins || "*",
+    origin: config.allowed_origins || '*',
     credentials: true,
   })
 );
@@ -26,13 +26,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //file retrieve
-app.use(express.static("uploads"));
-app.use(express.static("public"));
+app.use(express.static('uploads'));
+app.use(express.static('public'));
 
 //router
-app.use("/api/v1", router);
+app.use('/api/v1', router);
 //live response
-app.get("/", (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send(welcome());
 });
 
